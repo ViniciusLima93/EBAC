7e63eb9a2118aadc8b7c2d161d05a9a8074a41d9
ViniciusLima93 <vnlima9@gmail.com>
2024-01-28 14:37:41 -0400

    integrated-project2

---
+++ IntegratedProject/client/src/components/Properties/index.js
@@ -0,0 +1,237 @@
+import React, { Component, useState } from 'react'
+import axios from 'axios'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+
+const baseUrl = 'http://localhost:3333/properties'
+
+const initialState = {
+    propertie: {
+        _id: '',
+        type: '',
+        title: '',
+        description: ''
+    },
+    properties: []
+}
+
+export default class Properties extends Component {
+
+    /* Define estado inicial*/
+    constructor(props) {
+        super(props)
+        this.state = { ...initialState }
+    }
+    
+    /* Limpa formulário */
+    clear(){
+        this.setState({ propertie : initialState.propertie })
+    }
+
+    /* Cria Imóvel */
+    save(){
+        const propertie= this.state.propertie
+        const method  = propertie._id? 'put' : 'post'
+        const url = propertie._id? `${baseUrl}/${propertie._id }` : `${baseUrl}`
+        axios[method](url, propertie)
+            .then(response => {
+                // promisse
+                const list = this.getUpdatedList(response.data)
+                this.setState({ propertie: initialState.propertie, list })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
+
+    /* Atualiza lista de imóveis */
+    getUpdatedList(propertie){
+        const list = this.state.list?.filter(p => p._id !== propertie._id)
+        if(propertie) list.unshift(propertie)
+        return list
+    }
+
+    /* Atualiza Imóvel */
+    update(event) {
+        const propertie = { ...this.state.propertie }
+        propertie[event.target.name] = event.target.value
+        this.setState({ propertie })
+    }
+
+    componentDidMount(){
+        axios.get(baseUrl)
+            .then(response => {
+                this.setState({ list: response.data })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    /* Atualiza lista de imóveis */
+    // eslint-disable-next-line no-dupe-class-members
+    getUpdatedList(propertie){
+        console.log(propertie)
+        const list = this.state.list.filter(p => p._id !== propertie._id)
+        if(propertie) list.unshift(propertie)
+        return list
+    }
+
+    /* Lista Imóveis */
+    mapProperties(properties){
+        return this.state.list?.map( response  => {
+            return this.getListItem(response)
+        })
+    }
+
+    renderProperties(){
+        const properties = this.state.list
+        return ( <List>{ this.mapProperties(properties) }</List>)
+    }
+
+    getListItem(response){
+
+        return (
+            <ListItem  
+                alignItems="flex-start"
+                key={response._id}>
+                    <ListItemAvatar>
+                        <Avatar alt="Foto" src="https://picsum.photos/200/300?random=1" />
+                    </ListItemAvatar>
+                    <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            <Tooltip title={ response.type }>
+                                <h3>{response.title}</h3> 
+                            </Tooltip>
+                            <p>{response.description}</p>
+                        </Typography>
+                    </React.Fragment>
+                    <Divider variant="inset" component="li" />
+                    <div className='actions'>
+                        <button
+                            className='btn-edit'
+                            onClick={ () => this.load(response)}>
+                            Alterar</button>
+            
+                        <button
+                            className='btn-delete'
+                            onClick={ () => this.remove(response)}>
+                            Deletar</button>
+                    </div>
+            </ListItem>
+        )
+
+    }
+
+    /* Carrega Imóvel */
+    load(propertie) {
+        this.setState({ propertie })
+    }
+
+    /* Remove Imóvel */
+    remove(propertie) {
+        console.log(propertie)
+        axios.delete(`${baseUrl}/${propertie._id }`)
+            .then(response => {
+                // const list = this.state.list.filter( p => p._id !== propertie._id )
+                const list = this.getUpdatedList(null)
+                this.setState({ list })
+            })
+    }
+
+     /* Renderiza formulário de cadastro */
+    renderForm() {
+      return (
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '20px',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            borderRadius: '8px',
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {this.state.propertie._id}
+          <TextField
+            variant="outlined"
+            type="text"
+            name="title"
+            value={this.state.propertie.title}
+            onChange={(e) => this.update(e)}
+            label="Título do Anúncio"
+            style={{ marginBottom: '10px' }}
+          />
+  
+          <TextField
+            variant="outlined"
+            type="text"
+            name="type"
+            value={this.state.propertie.type}
+            onChange={(e) => this.update(e)}
+            label="Tipo do Imóvel"
+            style={{ marginBottom: '10px' }}
+          />
+  
+          <TextField
+            variant="outlined"
+            type="text"
+            name="description"
+            value={this.state.propertie.description}
+            onChange={(e) => this.update(e)}
+            label="Descrição"
+            multiline
+            rows={3}
+            style={{ marginBottom: '20px' }}
+          />
+  
+          <Stack spacing={2} direction="row">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(e) => this.save(e)}
+            >
+              Salvar
+            </Button>
+            <Button
+              variant="text"
+              color="secondary"
+              onClick={(e) => this.clear(e)}
+            >
+              Cancelar
+            </Button>
+          </Stack>
+        </Box>
+      );
+    }
+
+    /* Renderiza formulário e lista */
+    render() {
+        console.log(this.state.list)
+        return( 
+            <div className='content'>
+                {this.renderForm()}
+                {this.renderProperties()}
+            </div>)
+    }
+
+}
--- IntegratedProject/server/index.js
+++ IntegratedProject/server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 require('./config/db')
 
 const app = express();
 const port = 3333;
 
+app.use(cors());
 app.use(express.json())
 app.use(routes)
+
 
 app.listen(port, () => {
     console.log(`Running in port: ${port}`)
